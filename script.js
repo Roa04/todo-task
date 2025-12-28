afd538e7cc7a6d3b62e9e469853344dcc18ffa8b
Roa04 <130482586+Roa04@users.noreply.github.com>
2025-12-28 13:02:54 +0200

    implement task filtering in renderTodos function

--- script.js
+++ script.js
@@ -29,3 +29,37 @@ let currentFilter = "All";
 let taskToDeleteId = null;
 let currentTaskId = null;
 
+// Display tasks by filter
+function renderTodos(filter = "All") {
+  section.querySelectorAll(".task").forEach(task => task.remove());
+
+  let filtered = todos;
+  if (filter === "Done") {
+    filtered = todos.filter(todo => todo.done);
+  } else if (filter === "Todo") {
+    filtered = todos.filter(todo => !todo.done);
+  }
+
+  noTasksMessage.style.display = filtered.length ? "none" : "block";
+
+  filtered.forEach(todo => {
+    const taskEl = document.createElement("div");
+    taskEl.className = "task";
+
+    const checked = todo.done ? "checked" : "";
+    const doneClass = todo.done ? "done-task" : "";
+
+    taskEl.innerHTML = `
+      <div class="task-content">
+        <span class="${doneClass}">${todo.text}</span>
+        <div class="task-actions">
+          <input type="checkbox" ${checked} onchange="toggleDone(${todo.id})">
+          <i class="fas fa-pen edit-icon" onclick="editTodo(${todo.id})"></i>
+          <i class="fas fa-trash delete-icon" onclick="deleteTodo(${todo.id})"></i>
+        </div>
+      </div>
+    `;
+
+    section.appendChild(taskEl);
+  });
+}
